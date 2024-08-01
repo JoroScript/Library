f6d6159da620b68bee716b3a7422e45287ebdb67
JoroScript <gorko7878@abv.bg>
2024-08-02 01:11:50 +0300

    done

--- code.js
+++ code.js
@@ -6,7 +6,8 @@ let pages = document.querySelector('#pages');
 let hasRead = document.querySelector('#hasRead');
 const form = document.querySelector('form');
 const cardsContainer= document.querySelector('.cards');
-
+const deleteConfirm=document.querySelector('#deleteConfirm');
+let cardIndex=0;
 let Books = [
 
     // {
@@ -31,6 +32,7 @@ Book.prototype.ChangeRead = function(){
 function displayLibrary(){ 
   cardsContainer.innerHTML= '';
   Books.forEach(newBook => {
+    cardIndex=Books.indexOf(newBook);
     let titlePara = document.createElement('p');
     titlePara.textContent=`Title: ${newBook.title}`;
     let authorPara = document.createElement('p');
@@ -41,6 +43,7 @@ function displayLibrary(){
     pagesPara.textContent=`Pages: ${newBook.pages}`;
     let hasReadPara = document.createElement('p');
     hasReadPara.textContent=`Has Read: ${newBook.read}`;
+    hasReadPara.style.color = newBook.read ? 'green' : 'red'
     let delBtn=document.createElement('button');
     let changeReadBtn=document.createElement('button');
     changeReadBtn.className="changeRead";
@@ -49,18 +52,8 @@ function displayLibrary(){
       newBook.ChangeRead();
       displayLibrary();
     })
-    delBtn.addEventListener('click',(event)=>{
-      console.log(Books);
-      let cardIndex=Books.indexOf(newBook);
-        if (cardIndex !== -1) {
-          Books.splice(cardIndex, 1);
-          card.remove();
-      }
-      console.log(Books);
-        
-    })
-    delBtn.textContent='Delete';
-    changeReadBtn.textContent='Change Read';
+    delBtn.innerHTML="<div class='frame'><img src='https://cdn-icons-png.flaticon.com/512/2891/2891491.png'></div>";
+    changeReadBtn.innerHTML="<div class='frame'><img src='https://cdn-icons-png.flaticon.com/512/3068/3068380.png'></div>";
     let card = document.createElement('div');
     card.className="card";
     card.appendChild(titlePara);
@@ -73,7 +66,45 @@ function displayLibrary(){
     cardsContainer.appendChild(card);
     if (document.body.scrollHeight > window.innerHeight) {
       document.body.style.height = '100%';
-  }
+  } 
+
+    delBtn.addEventListener('click',()=>{
+      form.style.filter = 'blur(5px)';
+      cardsContainer.style.filter='blur(5px)';
+      let deleteConfirm = document.createElement('div');
+      deleteConfirm.style.filter="none";
+      deleteConfirm.className="deleteConfirm";
+      let deleteText=document.createElement('h3');
+      deleteText.innerHTML=`Are You Sure You Want To Delete <span>${newBook.title}</span>`
+      let yesDel=document.createElement('button');
+      yesDel.textContent="Yes";
+   
+      yesDel.className="yesDel";
+    
+      let noDel=document.createElement('button');
+      noDel.textContent="NO";
+      noDel.className="noDel";
+
+      deleteConfirm.appendChild(deleteText);
+      deleteConfirm.appendChild(yesDel);
+      deleteConfirm.appendChild(noDel);
+      document.body.appendChild(deleteConfirm);
+      yesDel.addEventListener('click',()=>{
+        form.style.filter = 'none';
+        cardsContainer.style.filter='none';
+        card.remove();
+        let cardIndex=Books.indexOf(newBook);
+        Books.splice(cardIndex,1);
+        deleteConfirm.remove();
+        displayLibrary();
+      })  
+      noDel.addEventListener('click',()=>{
+        deleteConfirm.remove();
+        form.style.filter = 'none';
+        cardsContainer.style.filter='none';
+      })
+    })
+
    
   });
 }
@@ -81,7 +112,7 @@ form.addEventListener('submit',function(event){
     event.preventDefault();
     let newBook= new Book(title.value,author.value,pages.value,language.value,hasRead.checked);
     Books.push(newBook);
-    displayLibrary();
+    displayLibrary(); 
   this.reset();
 })
 function Book(title,author,pages,language,read){
